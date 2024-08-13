cffcc84060a8e81b3a336a46af3813462e0902dd
Your Name <you@example.com>
2024-08-13 09:12:05 +0530

    Add 13/8

--- routes/index.js
+++ routes/index.js
@@ -121,6 +121,10 @@ router.patch('/:id', async function (req, res, next) {
 
     req.body.password = await bcrypt.hash(req.body.password, 10)
     const userUpdate = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    console.log(userUpdate);
+    console.log("===>>", req.params.id);
+    console.log("===>>====>>", req.body);
+
 
     res.status(200).json({                             //OK
       status: "success",
